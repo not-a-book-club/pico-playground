db51c75364d2ab929001853914bf1a87ed9bc136
Chris <chrisbutler296@gmail.com>
2025-01-17 19:44:25 -0700

    Move some things to build.rs

--- pico/build.rs
+++ pico/build.rs
@@ -7,4 +7,14 @@ fn main() {
 
     println!("cargo:rustc-link-search={}", out.display());
     println!("cargo:rerun-if-changed=memory.x");
+
+    // `--nmagic` is required if memory section addresses are not aligned to 0x10000,
+    // for example the FLASH and RAM sections in your `memory.x`.
+    // See https://github.com/rust-embedded/cortex-m-quickstart/pull/95
+    println!("cargo:rustc-link-arg=--nmagic");
+
+    // Set the linker script to the one provided by cortex-m-rt.
+    println!("cargo:rustc-link-arg=-Tlink.x");
+
+    println!("cargo:rustc-link-arg=-Tdefmt.x");
 }
