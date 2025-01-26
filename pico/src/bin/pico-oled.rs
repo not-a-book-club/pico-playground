6d8e20118cd0b58120345ae4d4a6b6c003afa4a7
Chris <chrisbutler296@gmail.com>
2025-01-25 19:14:05 -0700

    Add a primitive system to render "screens" of unrelated content

--- pico/src/bin/pico-oled.rs
+++ pico/src/bin/pico-oled.rs
@@ -6,6 +6,7 @@
 extern crate alloc;
 use defmt_rtt as _;
 // use panic_probe as _;
+use alloc::format;
 
 // Embedded things
 use cortex_m::delay::Delay;
@@ -112,7 +113,7 @@ fn main() -> ! {
         let fplib_end = rom::fplib_end();
         info!(
             "fplib: {} bytes [0x{:08x}, 0x{:08x}]",
-            fplib_start.offset_from(fplib_end),
+            fplib_end.offset_from(fplib_start),
             fplib_start,
             fplib_end,
         );
@@ -155,7 +156,7 @@ fn main() -> ! {
         let fplib_end = rom::fplib_end();
         info!(
             "fplib: {} bytes [0x{:08x}, 0x{:08x}]",
-            fplib_start.offset_from(fplib_end),
+            fplib_end.offset_from(fplib_start),
             fplib_start,
             fplib_end,
         );
@@ -236,87 +237,210 @@ fn main() -> ! {
         display.clear_unset();
     }
 
+    #[derive(Copy, Clone, Debug)]
+    enum ScreenState {
+        Conway,
+        Info,
+        Filler,
+    }
+
+    impl ScreenState {
+        fn next(&mut self) {
+            *self = match self {
+                Self::Conway => Self::Info,
+                Self::Info => Self::Filler,
+                Self::Filler => Self::Conway,
+            }
+        }
+    }
+
     let style_text = MonoTextStyle::new(&ascii::FONT_5X8, oled::BinaryColor::On);
+    let style_text_tiny = MonoTextStyle::new(&ascii::FONT_4X6, oled::BinaryColor::On);
     let line_height = style_text.line_height() as i32;
     let _line_margin = line_height / 3;
 
     let mut rng = SmallRng::from_seed(core::array::from_fn(|_| 17));
     let mut sim = simulations::Life::new(oled::WIDTH as usize, oled::HEIGHT as usize);
-    sim.clear_random(&mut rng);
 
     let mut needs_refresh = true;
+    let mut state = ScreenState::Conway;
 
-    loop {
-        // led.set_high().unwrap();
-        // delay.delay_ms(100);
+    // loop {
+    //     led.set_high().unwrap();
+    //     delay.delay_ms(100);
+    //     led.set_low().unwrap();
+    //     delay.delay_ms(500);
+    // }
 
-        let a = btn_a.is_low().unwrap();
-        let b = btn_b.is_low().unwrap();
+    'screens: loop {
+        match state {
+            ScreenState::Conway => {
+                display.clear_unset();
+                display.flush();
+                sim.clear_random(&mut rng);
 
-        match (a, b) {
-            // Reset back to BOOTSEL so that the next cargo-run updates our code
-            (true, true) => hal::rom_data::reset_to_usb_boot(0, 0),
+                loop {
+                    let a = btn_a.is_low().unwrap();
+                    let b = btn_b.is_low().unwrap();
 
-            // Press A to clear to random
-            (true, _) => sim.clear_random(&mut rng),
+                    match (a, b) {
+                        // Reset back to BOOTSEL so that the next cargo-run updates our code
+                        (true, true) => hal::rom_data::reset_to_usb_boot(0, 0),
 
-            // Press B to spawn random circles
-            (_, true) => {
-                use rand::Rng;
-                let n = 10;
-                let xx: i16 = rng.gen_range(2 * n..sim.width()) - n;
-                let yy: i16 = rng.gen_range(2 * n..sim.height()) - n;
-                for y in (yy - n)..(yy + n) {
-                    for x in (xx - n)..(xx + n) {
-                        let dist = (x - xx).abs() + (y - yy).abs();
-                        if dist <= n && dist % 3 == 0 {
-                            sim.set(x, y, true);
+                        // Press A to go to the next screen
+                        (true, _) => {
+                            state.next();
+                            delay.delay_ms(500);
+                            continue 'screens;
                         }
+
+                        // Press B to spawn random circles
+                        (_, true) => {
+                            use rand::Rng;
+                            let n = 10;
+                            let xx: i16 = rng.gen_range(2 * n..sim.width()) - n;
+                            let yy: i16 = rng.gen_range(2 * n..sim.height()) - n;
+                            for y in (yy - n)..(yy + n) {
+                                for x in (xx - n)..(xx + n) {
+                                    let dist = (x - xx).abs() + (y - yy).abs();
+                                    if dist <= n && dist % 3 == 0 {
+                                        sim.set(x, y, true);
+                                    }
+                                }
+                            }
+                        }
+
+                        _ => {}
+                    }
+
+                    // let n_updated = 0;
+                    let n_updated = sim.step();
+                    if n_updated != 0 {
+                        needs_refresh = true;
+                    }
+
+                    // Draw!
+                    if needs_refresh {
+                        let base_y = (oled::HEIGHT as u32 - view_height) as i32;
+
+                        // Draw a nice title
+                        let text = Text::new(
+                            "Conway's Game of Life",
+                            Point::new(3, base_y - 3),
+                            style_text,
+                        );
+                        let _ = text.draw(&mut display);
+
+                        // Draw our sim "to" the view
+                        for y in (base_y as i16 + 3)..(sim.height() - 3) {
+                            for x in 3..(sim.width() - 3) {
+                                let is_alive = sim.get(x, y);
+                                display.set(x, y, is_alive);
+                            }
+                        }
+
+                        // Draw border around our view
+                        let _ = RoundedRectangle::with_equal_corners(
+                            Rectangle::new(
+                                Point::new(0, base_y),
+                                Size::new(view_width, view_height),
+                            ),
+                            Size::new(5, 5),
+                        )
+                        .draw_styled(&style_white_border, &mut display);
+
+                        display.flush();
+                        needs_refresh = false;
                     }
                 }
             }
+            ScreenState::Info => {
+                unsafe {
+                    use hal::rom_data as rom;
 
-            _ => {}
-        }
+                    let copyright =
+                        Text::new(rom::copyright_string(), Point::zero(), style_text_tiny);
+                    // let rom_version =  Text::new(rom::rom_version_number(), Point::zero(), style_text_tiny);
 
-        // let n_updated = 0;
-        let n_updated = sim.step();
-        if n_updated != 0 {
-            needs_refresh = true;
-        }
+                    let fplib_start = rom::fplib_start();
+                    let fplib_end = rom::fplib_end();
+                    let fplib_info = format!(
+                        "fplib: {} bytes [0x{:08x}, 0x{:08x}]",
+                        fplib_end.offset_from(fplib_start),
+                        fplib_start as usize,
+                        fplib_end as usize,
+                    );
+                    let fplib_info = Text::new(&fplib_info, Point::zero(), style_text_tiny);
 
-        // Draw!
-        if needs_refresh {
-            let base_y = (oled::HEIGHT as u32 - view_height) as i32;
+                    let bootrom_git = format!("{:08x}", rom::git_revision());
+                    let bootrom_git = Text::new(&bootrom_git, Point::zero(), style_text_tiny);
 
-            // Draw a nice title
-            let text = Text::new(
-                "Conway's Game of Life",
-                Point::new(3, base_y - 3),
-                style_text,
-            );
-            let _ = text.draw(&mut display);
+                    display.clear_unset();
+                    let _ = copyright.translate((0, 10).into()).draw(&mut display);
+                    let _ = fplib_info.translate((0, 20).into()).draw(&mut display);
+                    let _ = bootrom_git.translate((0, 30).into()).draw(&mut display);
+                    display.flush();
+                }
+                delay.delay_ms(1000);
 
-            // Draw our sim "to" the view
-            for y in (base_y as i16 + 3)..(sim.height() - 3) {
-                for x in 3..(sim.width() - 3) {
-                    let is_alive = sim.get(x, y);
-                    display.set(x, y, is_alive);
+                loop {
+                    let a = btn_a.is_low().unwrap();
+                    let b = btn_b.is_low().unwrap();
+
+                    match (a, b) {
+                        // Reset back to BOOTSEL so that the next cargo-run updates our code
+                        (true, true) => hal::rom_data::reset_to_usb_boot(0, 0),
+
+                        // Press A to go to the next screen
+                        (true, _) => {
+                            state.next();
+                            continue 'screens;
+                        }
+
+                        (_, true) => {
+                            //
+                        }
+
+                        _ => {}
+                    }
+
+                    delay.delay_ms(100);
                 }
             }
 
-            // Draw border around our view
-            let _ = RoundedRectangle::with_equal_corners(
-                Rectangle::new(Point::new(0, base_y), Size::new(view_width, view_height)),
-                Size::new(5, 5),
-            )
-            .draw_styled(&style_white_border, &mut display);
+            // Copy and Paste this block when adding a new "screen"
+            _ => {
+                display.clear_unset();
+                let text = format!("TODO: {state:?}");
+                let _ = Text::new(&text, Point::new(16, 16), style_text).draw(&mut display);
+                display.flush();
+                delay.delay_ms(1000);
 
-            display.flush();
-            needs_refresh = false;
+                loop {
+                    let a = btn_a.is_low().unwrap();
+                    let b = btn_b.is_low().unwrap();
+
+                    match (a, b) {
+                        // Reset back to BOOTSEL so that the next cargo-run updates our code
+                        (true, true) => hal::rom_data::reset_to_usb_boot(0, 0),
+
+                        // Press A to go to the next screen
+                        (true, _) => {
+                            state.next();
+                            delay.delay_ms(500);
+                            continue 'screens;
+                        }
+
+                        (_, true) => {
+                            //
+                        }
+
+                        _ => {}
+                    }
+
+                    delay.delay_ms(100);
+                }
+            }
         }
-
-        // led.set_low().unwrap();
-        // delay.delay_ms(500);
     }
 }
